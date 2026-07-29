195fa2afdd197529c755e7475c807f55bae264d3
pbain63 <prodipbain63@gmail.com>
2026-07-29 09:37:30 +0600

    Add JS code for email input in main.js

--- main.js
+++ main.js
@@ -2,10 +2,14 @@ const form = document.querySelector("form");
 const yourName = document.getElementById("name");
 const yourNameError = document.querySelector("#name + span.error");
 
+const email = document.getElementById("mail");
+const emailError = document.querySelector("#mail + span.email-error");
+
+// Name
 yourName.addEventListener("input", (event) => {
   if (yourName.validity.valid) {
     yourNameError.textContent = "";
-    yourNameError.textContent = "error";
+    // yourNameError.textContent = "error";
   } else {
     showError();
   }
@@ -22,5 +26,35 @@ function showError() {
   if (yourName.validity.valueMissing) {
     yourNameError.textContent = "Please enter your name.";
   }
-  yourNameError.className = "error active";
+  //   yourNameError.className = "error active";
+}
+
+// Email
+
+email.addEventListener("input", (event) => {
+  if (email.validity.valid) {
+    emailError.textContent = "";
+    emailError.className = "email-error";
+  } else {
+    emailShowError();
+  }
+});
+
+form.addEventListener("submit", (event) => {
+  if (!email.validity.valid) {
+    emailShowError();
+    event.preventDefault();
+  }
+});
+
+function emailShowError() {
+  if (email.validity.valueMissing) {
+    emailError.textContent = "You need to enter an email address.";
+  } else if (email.validity.typeMismatch) {
+    emailError.textContent = "Entered value needs to be an email address.";
+  } else if (email.validity.tooShort) {
+    emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+  }
+
+  emailError.className = "email-error active";
 }
